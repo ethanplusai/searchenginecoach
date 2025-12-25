dc76231be86b6f55ce5189612019a614e535b865
Ethan <erogers999@gmail.com>
2025-12-25 01:12:14 -0500

    prepare for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://searchenginecoach.com',
   output: 'static',
+  adapter: cloudflare(),
   build: {
     format: 'directory'
   },
