c999dd75db22707b8ca893deb7b9883bdf37d54a
Ethan <e@ethanplus.ai>
2025-12-27 21:55:12 -0500

    troubleshooting form issue in prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://searchenginecoach.com',
   output: 'static',
+  adapter: vercel(),
   integrations: [sitemap()],
   build: {
     format: 'directory'
